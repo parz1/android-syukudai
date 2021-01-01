89002b6e51010a7e55bede49a5516bebc08741e7
parz1 <parz1zhou@gmail.com>
2021-01-01 22:03:43 +0800

    Add dice fun in Home frag

--- app/src/main/java/com/example/hw_activity/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/hw_activity/ui/home/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -26,6 +28,11 @@ class HomeFragment : Fragment() {
     homeViewModel.text.observe(viewLifecycleOwner, Observer {
       textView.text = it
     })
+    val rollButton: Button = root.findViewById(R.id.button)
+    rollButton.setOnClickListener {
+      val toast = Toast.makeText(activity, "Dice rolled!", Toast.LENGTH_SHORT)
+      toast.show()
+    }
     return root
   }
 }
